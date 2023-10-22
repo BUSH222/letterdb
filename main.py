abe156a1d8f5647d519115453f25ecaddafaa269
Ted Vt iMac <58144503+BUSH222@users.noreply.github.com>
2023-10-22 21:13:28 +0300

    layout changes, search function

--- main.py
+++ main.py
@@ -1,31 +1,49 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, request
 import os
 import sqlite3
+import re
 
 
 app = Flask(__name__)
-DB_REL_DIR = 'db' # fix later
+DB_FILE = 'maindb.db'
 DOCTXT_REL_DIR = 'documents-txt'
 DOC_REL_DIR = 'documents'
-# LOAD DATA AT START
+
 print('Letter data loading...')
-con = sqlite3.connect("maindb.db")
+
+# LOAD DATA AT START
+con = sqlite3.connect(DB_FILE)
 cur = con.cursor()
 con.row_factory = sqlite3.Row
 cur.execute("SELECT * FROM letterdb")
+
+# Data processing
 DATA = cur.fetchall()
-#DATA.sort(key=lambda x:x[0])
+DATA.sort(key=lambda x:x[0])
+DATADICT = []
+for elem in DATA:
+    DATADICT.append({"catalogue": elem[0], "date":elem[1], "addressee": elem[2], "keywords": elem[3], "regions":elem[4]})
+
 print('Letter data loaded.')
 
+
+def filter_data(data, filter_dict):
+    filtered_data = []
+    for d in data:
+        match = True
+        for key, value in filter_dict.items():
+            if value != "" and not re.match(value.replace('*', '.*'), str(d.get(key))):
+                match = False
+                break
+        if match:
+            filtered_data.append(d)
+    return filtered_data
 
 
 @app.route('/')
 def index():
-    global DATA
-    findata = []
-    for elem in DATA:
-        findata.append({"catalogue": elem[0], "date":elem[1], "addressee": elem[2], "keywords": elem[3], "regions":elem[4]})
-    return render_template('index.html', data=findata)
+    global DATADICT
+    return render_template('index.html', data=DATADICT)
 
 
 @app.route('/viewitem/<string:itemindex>')
@@ -34,6 +52,13 @@ def viewitem(itemindex):
         if os.path.isfile(os.path.join(DOCTXT_REL_DIR, s)) and s.startswith(itemindex):
             return send_file(os.path.join(DOC_REL_DIR, f'{s.rsplit(".", 1)[0]}.doc'))
     return "NO FILE FOUND"
+
+@app.route('/search', methods=["POST"])
+def search():
+    global DATADICT
+    query = dict(request.form)
+    print(query)
+    return render_template('index.html', data=filter_data(DATADICT, query))
 
 
 if __name__ == '__main__':
